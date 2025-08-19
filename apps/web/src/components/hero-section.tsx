8d107d96653ac29637d69744f8fd24e65c23400d
titanium-dev <akinkunmioye42@gmail.com>
2025-08-19 07:24:54 +0100

    Add HeroSection component to showcase ReactServe features

---
+++ apps/web/src/components/hero-section.tsx
@@ -0,0 +1,63 @@
+import { Link } from "@tanstack/react-router";
+import { SiReact } from "react-icons/si";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { tomorrow } from "react-syntax-highlighter/dist/esm/styles/prism";
+
+export function HeroSection() {
+  return (
+    <div className="flex items-center px-32 mt-40 gap-x-32">
+      <div className="">
+        <h2 className="text-5xl font-light">
+          The missing backend <br /> framework for React.
+        </h2>
+        <div className="bg-white/5 border p-3 border-white/7 mt-5">
+          <code className="text-sm">$ npx create-react-serve@latest</code>
+        </div>
+        <Link to="/docs/at-a-glance">
+          <button className="mt-5 bg-white text-black px-5 py-3 text-sm cursor-pointer">
+            Learn ReactServe
+          </button>
+        </Link>
+      </div>
+      <div className="p-4 ml-10 flex-1 border border-white/10">
+        <div className="text-xs px-4 py-2 flex gap-x-1 items-center border border-white/10 w-fit bg-white/4">
+          <SiReact size={14} color="#61DAFB" />
+          <span className="text-zinc-400">backend.tsx</span>
+        </div>
+        <div className="ext-sm font-mono p-4 rounded-b-lg">
+          <SyntaxHighlighter
+            language="jsx"
+            style={tomorrow}
+            customStyle={{
+              background: "transparent",
+              padding: 0,
+              margin: 0,
+              fontSize: "0.875rem",
+            }}
+          >
+            {`<App port={6969}>
+  <Route path="/" method="GET">
+    {async () => {
+      return <Response json={{ message: "Hello World!" }} />;
+    }}
+  </Route>
+
+  <RouteGroup prefix="/api">
+    <Middleware use={authMiddleware} />
+
+    <Route path="/users/:id" method="GET">
+      {async () => {
+        const { params } = useRoute();
+        const user = useContext("user");
+        return <Response json={{ userId: params.id, currentUser: user }} />;
+      }}
+    </Route>
+  </RouteGroup>
+</App>`}
+          </SyntaxHighlighter>
+        </div>
+        <div className="mt-4 flex justify-end"></div>
+      </div>
+    </div>
+  );
+}
